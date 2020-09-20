31f7e611f8d4b1d4e953318db41199c87cb85a86
landryfomena <landryfomena09@gmail.com>
2020-09-20 01:24:56 +0100

    initial commit

---
+++ src/main/java/com/example/landbanking/LandbankingApplication.java
@@ -0,0 +1,13 @@
+package com.example.landbanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LandbankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LandbankingApplication.class, args);
+	}
+
+}
